c8683bb82bbb5d6edbe5c74964d9ff4237ca37c2
thaghung <reddevill.8989@gmail.com>
2023-09-15 03:17:16 +0700

    Buoi3_TranTheAn

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿class BTVNBuoi3 {
     public static void Main() {
-        Console.WriteLine("Ket qua bai 1: " + Bai1("03:03:00PM"));
+        Console.Write("Nhập thời gian theo định dạng hh:mm:ss tt (ví dụ: 03:03:00 PM): ");
+        string ampm = Console.ReadLine();
+
+        string result = Bai1(ampm);
+        Console.WriteLine("Ket qua bai 1: " + result);
 
         string[] name = {"A", "B", "C", "D", "E"};
         float[] grade = {3.8F, 7.9F, 6.0F, 2.8F, 5.0F};
@@ -9,7 +13,16 @@
 
     public static string Bai1(string ampm)
     {
-        return "";
+        try
+        {
+            DateTime dateTime = DateTime.ParseExact(ampm, "hh:mm:ss tt", null);
+            string result = dateTime.ToString("HH:mm:ss");
+            return result;
+        }
+        catch (FormatException)
+        {
+            return "Định dạng đầu vào không hợp lệ.";
+        }
     }
 
     public static string Bai2(string[] name, float[] grade)
