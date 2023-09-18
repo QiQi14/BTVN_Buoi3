f696bf96997457e3104a6bb8d3530ec002e9656e
ToanNguyen995 <toan03031995@gmail.com>
2023-09-18 17:51:20 +0700

    commit BTVNbuoi3_bai1

---
+++ BTVNbuoi3.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace BTVNbuoi3_Bai1
+{
+    public class BTVNbuoi3
+    {
+        public static void Main(string[] args)
+        {
+
+            DateTime d = DateTime.Parse("10:00 PM");
+            Console.WriteLine(d.ToString("HH:mm"));
+
+        }
+    }
+}
