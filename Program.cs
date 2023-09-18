f92222b630fd42a81caa38b3a8716f1f1885ff19
quyphung994 <quypn994@gmail.com>
2023-09-18 17:05:51 +0700

    ket qua bai tap buoi 3

--- Program.cs
+++ Program.cs
@@ -7,13 +7,39 @@
         Console.WriteLine("Ket qua bai 2: " + Bai2(name, grade));
     }
 
-    public static string Bai1(string ampm)
+    public static string Bai1(string s)
     {
-        return "";
+        string ampm = s.Substring(s.Length - 2, 2);
+        int hour = Convert.ToInt32(s.Substring(0, 2));
+        string body = s.Substring(2, s.Length - 4);
+        if (ampm.Equals("PM"))
+        {
+            if (hour < 12)
+                hour += 12;
+        }
+        else
+        {
+            if (hour == 12)
+                hour = 0;
+        }
+        return "" + hour + body;
     }
 
-    public static string Bai2(string[] name, float[] grade)
+    public static string Bai2(string[] names, float[] grades)
     {
-        return "";
+        for (int i = 0; i < grades.Length; i++)
+        {
+            if (grades[i] < 3.8F)
+                continue;
+
+            float different = (grades[i] * 10 % 5) * 0.1F;
+            if (different > 0.2)
+            {
+                float roundedGrade = grades[i] + 0.5F - different;
+                grades[i] = (float)Math.Round(roundedGrade, 2);
+            }
+        }
+        string result = string.Join(",", names) + "\n" + string.Join(",", grades);
+        return result;
     }
 }
